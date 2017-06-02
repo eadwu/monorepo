1a583f24b484690b0654128a5de80e40f0beb216
eadwu <fangkazuto@gmail.com>
2017-06-01 21:20:29 -0400

    Improve automaton script

--- public/arch-x-os/scripts/install.bash
+++ public/arch-x-os/scripts/install.bash
@@ -1,23 +1,19 @@
-#!/usr/bin/bash
-
-# Usage
-# Get the script then run it
-# Example
-# curl "https://raw.githubusercontent.com/arch-dual-boot/arch-x-os/master/scripts/install.sh" | bash abc123 US /America/New_York bahamut abc123 derp abc123
+#!/usr/bin/env bash
 
 # Variables
-lvmPassword=$1
+cryptPassword=$1
 country=$2 # From https://www.archlinux.org/mirrorlist
 geographicZone=$3 # Format as /:Area/:Region like /America/New_York
 hostname=$4
 rootPassword=$5
 user=$6
 password=$7
+locale=$8
 
 # Partitioning
-cgdisk /dev/sda
-(echo "YES"; echo $lvmPassword; echo $lvmPassword) | cryptsetup -c aes-xts-plain64 --use-random luksFormat /dev/sda6
-echo $lvmPassword | cryptsetup open --type luks /dev/sda6 lvm
+cgdisk /dev/sda < /dev/tty
+printf $cryptPassword | cryptsetup -c aes-xts-plain64 --use-random luksFormat /dev/sda6 -
+printf $cryptPassword | cryptsetup open --type luks /dev/sda6 lvm -
 pvcreate --dataalignment 1m /dev/mapper/lvm
 vgcreate volgroup0 /dev/mapper/lvm
 lvcreate -L 2G volgroup0 -n lv_swap
@@ -41,34 +37,42 @@ sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.source
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.source > /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel
 genfstab -U -p /mnt >> /mnt/etc/fstab
-nano /mnt/etc/fstab
+nano /mnt/etc/fstab < /dev/tty
 
-# Arch-chroot script
-cat <<EOF > /mnt/chroot.sh
+# Instructions to execute in arch-chroot
+cat <<SOF > /mnt/chroot.sh
 (echo $rootPassword; echo $rootPassword) | passwd
-pacman -S wpa_supplicant wireless_tools linux-headers
+pacman -S wpa_supplicant wireless_tools linux-headers < /dev/tty
 echo $hostname > /etc/hostname
 ln -sf /usr/share/zoneinfo$geographicZone /etc/localtime
 hwclock --systohc --utc
 useradd -m -g users -G wheel -s /bin/bash $user
 (echo $password; echo $password) | passwd $user
 echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/10-grant-wheel-group
-nano /etc/locale.gen
+sed -i "s/^#$locale UTF-8/$locale UTF-8/" /etc/locale.gen
 locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-export LANG=en_US.UTF-8
-nano /etc/mkinitcpio.conf
+echo LANG=$locale > /etc/locale.conf
+export LANG=$locale
+sed -i 's/^HOOKS="base udev autodetect modconf block filesystems keyboard fsck"/HOOKS="base udev autodetect keyboard modconf block encrypt lvm2 filesystems fsck"/' /etc/mkinitcpio.conf
 mkinitcpio -p linux
-pacman -S grub-efi-x86_64
-nano /etc/default/grub
+pacman -S grub-efi-x86_64 < /dev/tty
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=\/dev\/sda6:volgroup0 quiet rootflags=data=writeback libata.force=1:noncq"/' /etc/default/grub
+cat <<EOF >> /etc/default/grub
+
+# Fix broken grub.cfg gen
+GRUB_DISABLE_SUBMENU=y
+EOF
 grub-mkconfig -o boot/grub/grub.cfg
 grub-mkstandalone -o boot.efi -d usr/lib/grub/x86_64-efi -O x86_64-efi --compress=xz boot/grub/grub.cfg
 mkdir /mnt/usbdisk && mount /dev/sdd1 /mnt/usbdisk
 cp boot.efi /mnt/usbdisk
 umount /mnt/usbdisk
+rm -rf boot.efi
 exit
-EOF
+SOF
 
+chmod +x /mnt/chroot.sh
 arch-chroot /mnt ./chroot.sh
+rm -rf /mnt/chroot.sh
 umount /mnt/boot
 umount /mnt
