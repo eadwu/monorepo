9590031591d648192d094171deff6aa963ba0bc2
Edmund Wu <fangkazuto@gmail.com>
2017-08-28 16:51:59 -0400

    Improve setup script
    Former-commit-id: 1d5ddb5d36fa431eb1361bebe846da6aa464988f

--- public/arch-x-os/scripts/setup.bash
+++ public/arch-x-os/scripts/setup.bash
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
-## Variables
+### Variables
 CONFIG=$HOME/.config
-DIRECTORY=$HOME/Downloads
+DOWNLOADS=$HOME/Downloads
 
-## Dotfiles
-curl -o $DIRECTORY/arch-x-os-master.zip https://codeload.github.com/arch-dual-boot/arch-x-os/zip/master
-# curl -Lo $DIRECTORY/atom-config.zip https://gist.github.com/eadwu/3cf21fa5a80c6902213a3e1960cd9d3b/archive/33307b1eed4b80bbff458fd8ae5155ba7dfbeb7d.zip
-unzip $DIRECTORY/arch-x-os-master.zip
-# unzip $DIRECTORY/atom-config.zip
-DOTFILES=$DIRECTORY/arch-x-os-master/dotfiles
-mv $DOTFILES/.bashrc $HOME
+### Zsh
+## Set Zsh as default shell
+chsh -s /bin/zsh
+## Use Pure Prompt for the console
+PURE_VERSION=$(curl https://api.github.com/repos/sindresorhus/pure/releases/latest | grep -e '\"tag_name\".*,' | cut -c 16- | rev | cut -c 3- | rev)
+curl -Lo $DOWNLOADS/pure.zip https://github.com/sindresorhus/pure/archive/$PURE_VERSION.zip
+unzip $DOWNLOADS/pure.zip
+PURE=$DOWNLOADS/pure-$PURE_VERSION
+mv $PURE/pure.zsh /usr/share/zsh/site-functions/prompt_pure_setup
+mv $PURE/async.zsh /usr/share/zsh/site-functions/async
+
+### Dotfiles
+ZIP_COMMIT=$(curl https://gitlab.com/api/v4/projects/arch-dual-boot%2Farch-x-os/repository/commits | grep -o '\"id\":\"[^,]*' | head -1 | cut -c 7- | rev | cut -c 2- | rev)
+curl -Lo $DOWNLOADS/arch-x-os.zip https://gitlab.com/arch-dual-boot/arch-x-os/repository/master/archive.zip
+unzip $DOWNLOADS/arch-x-os.zip
+
+DOTFILES=$DOWNLOADS/arch-x-os-master-$ZIP_COMMIT/dotfiles
+mv $DOTFILES/.zshrc $HOME
 mv $DOTFILES/.vimrc $HOME
 mv $DOTFILES/.Xresources $HOME
 mv $DOTFILES/.vim $HOME
@@ -23,21 +34,23 @@ mv $DOTFILES/i3lock.service /etc/systemd/system
 chmod +x /usr/local/bin/xflock4
 chmod +x $CONFIG/conky/init-conky
 
-## Fonts
-# curl -o $DIRECTORY/input-font.zip http://input.fontbureau.com/build/?fontSelection=fourStyleFamily&regular=InputMonoNarrow-Light&italic=InputMonoNarrow-LightItalic&bold=InputMonoNarrow-Medium&boldItalic=InputMonoNarrow-MediumItalic&a=0&g=ss&i=serif&l=serifs_round&zero=slash&asterisk=height&braces=straight&preset=dejavu&line-height=1.2&accept=I+do&email=
-curl -o $DIRECTORY/awesome-font.zip http://fontawesome.io/assets/font-awesome-4.7.0.zip
-curl -o $DIRECTORY/flottflott-font.zip http://dl.dafont.com/dl/?f=flottflott
-curl -Lo $DIRECTORY/fira-code-font.zip https://github.com/tonsky/FiraCode/releases/download/1.204/FiraCode_1.204.zip
-# unzip $DIRECTORY/input-font.zip -d $DIRECTORY/Input
-unzip $DIRECTORY/awesome-font.zip
-unzip $DIRECTORY/flottflott-font.zip -d $DIRECTORY/flottflott
-unzip $DIRECTORY/fira-code-font.zip -d $DIRECTORY/fira-code
+systemctl daemon-reload
+systemctl enable i3lock.service
+systemctl start i3lock.service
+
+### Fonts
+## Operator and Input require a manual installation
+curl -o $DOWNLOADS/awesome-font.zip http://fontawesome.io/assets/font-awesome-4.7.0.zip
+curl -Lo $DOWNLOADS/fira-code-font.zip https://github.com/tonsky/FiraCode/releases/download/1.204/FiraCode_1.204.zip
+unzip $DOWNLOADS/awesome-font.zip
+unzip $DOWNLOADS/fira-code-font.zip -d $DOWNLOADS/fira-code
+
 mkdir -p $HOME/.local/share/fonts
-FONT=$HOME/.local/share/fonts
-mv $DIRECTORY/font-awesome-4.7.0/fonts/fontawesome-webfont.ttf $FONT/fontawesome-webfont.ttf
-mv $DIRECTORY/flottflott/Flottflott.ttf $FONT/Flottflott.ttf
-mv $DIRECTORY/fira-code/ttf/FiraCode-Bold.ttf $FONT/FiraCode-Bold.ttf
-mv $DIRECTORY/fira-code/ttf/FiraCode-Light.ttf $FONT/FiraCode-Light.ttf
-mv $DIRECTORY/fira-code/ttf/FiraCode-Medium.ttf $FONT/FiraCode-Medium.ttf
-mv $DIRECTORY/fira-code/ttf/FiraCode-Regular.ttf $FONT/FiraCode-Regular.ttf
-mv $DIRECTORY/fira-code/ttf/FiraCode-Retina.ttf $FONT/FiraCode-Retina.ttf
+FONTS=$HOME/.local/share/fonts
+
+mv $DOWNLOADS/font-awesome-4.7.0/fonts/FontAwesome.otf $FONTS
+mv $DOWNLOADS/fira-code/otf/FiraCode-Bold.otf $FONTS
+mv $DOWNLOADS/fira-code/otf/FiraCode-Light.otf $FONTS
+mv $DOWNLOADS/fira-code/otf/FiraCode-Medium.otf $FONTS
+mv $DOWNLOADS/fira-code/otf/FiraCode-Regular.otf $FONTS
+mv $DOWNLOADS/fira-code/otf/FiraCode-Retina.otf $FONTS
