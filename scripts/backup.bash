86c1f566128adf3ec89c270e334001d9eb5c4908
Edmund Wu <fangkazuto@gmail.com>
2017-08-29 21:08:34 -0400

    Remove unnecessary commands for backup
    Removed backup commands for i3status and tilix
    
    
    Former-commit-id: 533b6c3c6b781e11a4b30708160e1a1c0841e0ef

--- scripts/backup.bash
+++ scripts/backup.bash
@@ -5,8 +5,6 @@
 rm -rf dotfiles/.*
 rm -rf dotfiles/conky
 rm -rf dotfiles/i3
-rm -rf dotfiles/i3status
-rm -rf dotfiles/tilix
 rm -rf dotfiles/compton.conf
 # backup
 cp $HOME/.zshrc dotfiles/.zshrc
@@ -14,7 +12,6 @@ cp $HOME/.vimrc dotfiles/.vimrc
 cp $HOME/.Xresources dotfiles/.Xresources
 cp $HOME/.config/compton.conf dotfiles/compton.conf
 cp -r $HOME/.config/i3 dotfiles/i3
-cp -r $HOME/.config/i3status dotfiles/i3status
 cp -r $HOME/.config/conky dotfiles/conky
 
 mkdir dotfiles/.vim
@@ -22,10 +19,6 @@ mkdir dotfiles/.vim/autoload
 mkdir dotfiles/.vim/colors
 curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > dotfiles/.vim/autoload/plug.vim
 curl https://raw.githubusercontent.com/arcticicestudio/nord-vim/master/colors/nord.vim > dotfiles/.vim/colors/nord.vim
-
-mkdir dotfiles/tilix
-mkdir dotfiles/tilix/schemes
-curl https://raw.githubusercontent.com/arcticicestudio/nord-tilix/develop/src/json/nord.json > dotfiles/tilix/schemes/nord.json
 
 ## pkg lists
 pacman -Qqttn > pkg_lists/pacman.txt
