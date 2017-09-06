f6b6706edb46e95417d5c95e73e89732b922c687
eadwu <fangkazuto@gmail.com>
2017-09-06 17:24:07 -0400

    Rewrite based on new dotfiles/ structure
    Former-commit-id: cc0665ca55c09cacf2898353c8a3126fd9509398

--- scripts/backup.bash
+++ scripts/backup.bash
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
-## dotfiles
 # cleaning
 rm -rf dotfiles/.*
-rm -rf dotfiles/conky
-rm -rf dotfiles/i3
-rm -rf dotfiles/compton.conf
 # backup
-cp $HOME/.zshrc dotfiles/.zshrc
-cp $HOME/.vimrc dotfiles/.vimrc
-cp $HOME/.Xresources dotfiles/.Xresources
-cp $HOME/.config/compton.conf dotfiles/compton.conf
-cp -r $HOME/.config/i3 dotfiles/i3
-cp -r $HOME/.config/conky dotfiles/conky
+mkdir dotfiles/.config
+
+cp ${HOME}/.config/compton.conf dotfiles/.config
+cp -r ${HOME}/.config/conky dotfiles/conky
+cp -r ${HOME}/.config/gtk-3.0 dotfiles/gtk-3.0
+
+cp ${HOME}/.vimrc dotfiles
+cp ${HOME}/.gtkrc-2.0 dotfiles
+
+cp -r ${HOME}/.xmonad dotfiles/.xmonad
+
+cp ${HOME}/.Xresources dotfiles
 
 mkdir dotfiles/.vim
-mkdir dotfiles/.vim/autoload
 mkdir dotfiles/.vim/colors
+mkdir dotfiles/.vim/autoload
+mkdir dotfiles/.vim/autoload/airline
+mkdir dotfiles/.vim/autoload/airline/themes
+
 curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > dotfiles/.vim/autoload/plug.vim
 curl https://raw.githubusercontent.com/arcticicestudio/nord-vim/master/colors/nord.vim > dotfiles/.vim/colors/nord.vim
+curl https://raw.githubusercontent.com/arcticicestudio/nord-vim/develop/autoload/airline/themes/nord.vim > dotfiles/.vim/autoload/airline/themes/nord.vim
 
-## pkg lists
+cp ${HOME}/.zshrc dotfiles
+# packages
 pacman -Qqttn > pkg_lists/pacman.txt
 pacman -Qqttm > pkg_lists/yaourt.txt
