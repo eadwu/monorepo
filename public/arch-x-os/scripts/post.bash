8de5edfd8a416828fbb9f1ccb9e8edf8fb8caa4e
eadwu <fangkazuto@gmail.com>
2017-10-27 22:15:46 -0400

    Improve post installation process
    Former-commit-id: 4bf3e0c68666a15efdbbfd4f308a434441d6d657

--- public/arch-x-os/scripts/post.bash
+++ public/arch-x-os/scripts/post.bash
@@ -10,8 +10,10 @@
 virtualbox=${1} # |true
 
 # Pacman Repositories
-perl -0777 -i -pe 's/#(\[multilib\])\n#(Include = \/etc\/pacman.d\/mirrorlist)/\1\n\2\n\n\[archlinuxfr\]\nSigLevel = Never\nServer = http:\/\/repo.archlinux.fr\/\$arch\n\n\[herecura\]\nServer = https:\/\/repo.herecura.be\/herecura\/x86_64/' /etc/pacman.conf
-pacman -Sy
+perl -0777 -i -pe 's/(\[extra\]\nInclude = \/etc\/pacman.d\/mirrorlist)(.+?)#(\[multilib\]\n)#(Include = \/etc\/pacman.d\/mirrorlist)/\1\n\n\[haskell-core\]\nSigLevel = Required TrustedOnly\nServer = http:\/\/xsounds.org\/~haskell\/core\/\$arch\2\3\4\n\n\[archlinuxfr\]\nSigLevel = Never\nServer = http:\/\/repo.archlinux.fr\/\$arch\n\n\[herecura\]\nServer = https:\/\/repo.herecura.be\/herecura\/x86_64/s' /etc/pacman.conf
+pacman-key -r 4209170B
+pacman-key --lsign-key 4209170B
+pacman -Syy
 # Core Packages
 pacman -S \
   pulseaudio pulseaudio-alsa pulseaudio-bluetooth \
@@ -22,10 +24,11 @@ pacman -S \
   bluez bluez-libs bluez-utils
 # Graphical Environment
 pacman -S \
+  qt5-base qt5-doc \
   lightdm lightdm-gtk-greeter \
-  xmonad xmonad-contrib xmobar hlint \
+  haskell-xmonad haskell-xmonad-contrib xmobar haskell-hlint haskell-stack \
   compton thunar gvfs thunar-volman nitrogen rxvt-unicode \
-  xfce4-screenshooter xfce4-taskmanager
+  xfce4-notifyd xfce4-screenshooter xfce4-taskmanager
 # Additional Packages
 pacman -S \
   zsh vim \
@@ -33,10 +36,12 @@ pacman -S \
   cmake clang xclip openssh \
   broadcom-wl-dkms pepper-flash \
   powertop tlp cups avahi hplip \
-  git npm yarn nodejs php glslang \
   adapta-gtk-theme deepin-gtk-theme \
-  conky docker openvpn redshift rofi ranger feh cmus \
-  luarocks lm_sensors python-fonttools lsb-release scrot cmatrix \
+  scrot cmatrix simplescreenrecorder \
+  git npm yarn nodejs php glslang texlive-most \
+  java-runtime-common java-environment-common jdk8-openjdk jdk9-openjdk \
+  luarocks lm_sensors python-fonttools lsb-release \
+  fcron conky docker openvpn redshift rofi ranger feh cmus \
   blender opera-developer opera-developer-ffmpeg-codecs \
   noto-fonts-cjk ttf-liberation
 ## Virtualbox Guest Utils
@@ -53,10 +58,7 @@ fi
 perl -0777 -i -pe 's/hosts: (.+)(resolve \[!UNAVAIL=return\])(.+)\n/hosts: \1mdns_minimal \[NOTFOUND=return\] \2\3\n/' /etc/nsswitch.conf
 echo '
 ### Automatically switch to newly-connected devices
-load-module module-switch-on-connect
-
-### Support for audio in Docker containers
-load-module module-native-protocol-unix' >> /etc/pulse/default.pa
+load-module module-switch-on-connect' >> /etc/pulse/default.pa
 echo 'Section "InputClass"
   Identifier "touchpad"
   Driver "libinput"
@@ -67,6 +69,7 @@ echo 'Section "InputClass"
 EndSection' > /etc/X11/xorg.conf.d/30-touchpad.conf
 # Systemctl daemons/services
 systemctl daemon-reload
+systemctl enable fcron
 systemctl enable fstrim.timer
 systemctl enable ntpd
 systemctl enable bluetooth
