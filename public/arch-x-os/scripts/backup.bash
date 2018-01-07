e66bf5c866b107267a7d94ec0f954deb4368e7b4
eadwu <fangkazuto@gmail.com>
2018-01-07 13:28:39 -0500

    Update backup script
    Former-commit-id: aeab1f6e58793eb6d44e49cbf07c6ba9476185d7

--- public/arch-x-os/scripts/backup.bash
+++ public/arch-x-os/scripts/backup.bash
@@ -2,18 +2,18 @@
 
 # clean directory
 rm -rf dotfiles/.config
+rm -rf dotfiles/.ncmpcpp
 rm -rf dotfiles/.vim
 rm -rf dotfiles/etc
-rm -rf dotfiles/usr/local
 # directories
 mkdir dotfiles/.config
+mkdir dotfiles/.ncmpcpp
 mkdir dotfiles/.vim
 mkdir dotfiles/etc
 # variables
 CONFIG=dotfiles/.config
 VIM=dotfiles/.vim
 ETC=dotfiles/etc
-USR=dotfiles/usr
 
 # home
 cp ${HOME}/.gtkrc-2.0 dotfiles
@@ -26,11 +26,14 @@ cp ${HOME}/.config/compton.conf ${CONFIG}
 ###   gtk-3.0
 mkdir ${CONFIG}/gtk-3.0
 cp ${HOME}/.config/gtk-3.0/settings.ini ${CONFIG}/gtk-3.0
-###   vis
-mkdir ${CONFIG}/vis
-mkdir ${CONFIG}/vis/colors
-cp ${HOME}/.config/vis/config ${CONFIG}/vis
-cp ${HOME}/.config/vis/colors/colors ${CONFIG}/vis/colors
+###   mpd
+mkdir ${CONFIG}/mpd
+cp ${HOME}/.config/mpd/mpd.conf ${CONFIG}/mpd
+####    playlists
+mkdir ${CONFIG}/mpd/playlists
+
+##  .ncmpcpp
+cp ${HOME}/.ncmpcpp/config dotfiles/.ncmpcpp
 
 ##  .vim
 mkdir ${VIM}/autoload
@@ -48,27 +51,24 @@ curl https://raw.githubusercontent.com/arcticicestudio/nord-vim/master/colors/no
 
 ##  etc
 mkdir ${ETC}/lightdm
+mkdir ${ETC}/pulse
 mkdir ${ETC}/sysctl.d
 mkdir ${ETC}/systemd
 cp /etc/oblogout.conf ${ETC}
 ###   lightdm
 cp /etc/lightdm/lightdm.conf ${ETC}/lightdm
 cp /etc/lightdm/pantheon-greeter.conf ${ETC}/lightdm
+###   pulse
+cp /etc/pulse/client.conf ${ETC}/pulse
+cp /etc/pulse/default.pa ${ETC}/pulse
 ###   sysctl.d
 cp /etc/sysctl.d/30-max-user-instances.conf ${ETC}/sysctl.d
 cp /etc/sysctl.d/40-max-user-watches.conf ${ETC}/sysctl.d
 ###   systemd
-mkdir ${ETC}/systemd/system
 cp /etc/systemd/logind.conf ${ETC}/systemd
 ####    system
+mkdir ${ETC}/systemd/system
 cp /etc/systemd/system/i3color.service ${ETC}/systemd/system
-
-##  usr
-mkdir ${USR}/local
-###   local
-mkdir ${USR}/local/bin
-####    bin
-cp /usr/local/bin/i3color ${USR}/local/bin
 
 # package lists
 pacman -Qen | awk '{print $1}' > external/pacman.txt
