e3a8c26cbcb3d23b787388963e69df762344703a
Edmund Wu <fangkazuto@gmail.com>
2018-05-19 22:00:51 -0400

    Keep output filenames static
    Former-commit-id: 3b5df4f7355ab705bfea72a438465b2ec43b4b84

--- public/arch-x-os/scripts/post.bash
+++ public/arch-x-os/scripts/post.bash
@@ -14,14 +14,14 @@ perl -0777 -i -pe 's/#(\[multilib\]\n)#(Include = \/etc\/pacman.d\/mirrorlist)/\
 pacman -Syy
 pacman -Syu
 # Core Packages
-curl "https://gitlab.com/eadwu/pkgbuilds/raw/master/x86_64/kcore-meta-0.1.0-1-x86_64.pkg.tar.xz" -o kcore-meta-0.1.0-1-x86_64.pkg.tar.xz
-pacman -U kcore-meta-0.1.0-1-x86_64.pkg.tar.xz
+curl "https://gitlab.com/eadwu/pkgbuilds/raw/master/x86_64/kcore-meta-0.1.0-1-x86_64.pkg.tar.xz" -o kcore-meta.pkg.tar.xz
+pacman -U kcore-meta.pkg.tar.xz
 # Graphical Environment
-curl "https://gitlab.com/eadwu/pkgbuilds/raw/master/x86_64/kenv-meta-0.1.0-1-x86_64.pkg.tar.xz" -o kenv-meta-0.1.0-1-x86_64.pkg.tar.xz
-pacman -U kenv-meta-0.1.0-1-x86_64.pkg.tar.xz
+curl "https://gitlab.com/eadwu/pkgbuilds/raw/master/x86_64/kenv-meta-0.1.0-1-x86_64.pkg.tar.xz" -o kenv-meta.pkg.tar.xz
+pacman -U kenv-meta.pkg.tar.xz
 # Additional Packages
-curl "https://gitlab.com/eadwu/pkgbuilds/raw/master/x86_64/kother-meta-0.1.0-2-x86_64.pkg.tar.xz" -o kother-meta-0.1.0-2-x86_64.pkg.tar.xz
-pacman -U kother-meta-0.1.0-2-x86_64.pkg.tar.xz
+curl "https://gitlab.com/eadwu/pkgbuilds/raw/master/x86_64/kother-meta-0.1.0-2-x86_64.pkg.tar.xz" -o kother-meta.pkg.tar.xz
+pacman -U kother-meta.pkg.tar.xz
 ## Virtualbox Guest Utils
 if [ "${virtualbox}" = true ]; then
   pacman -S virtualbox-guest-utils
@@ -32,3 +32,5 @@ vboxvideo' > /etc/modules-load.d/virtualbox.conf
   cp /etc/X11/xinit/xinitrc ~/.xinitrc
   perl -0777 -i -pe 's/(#!\/bin\/sh)/\1\n\/usr\/bin\/VBoxClient-all/' ~/.xinitrc
 fi
+## Cleanup
+rm k*-meta.pkg.tar.xz
