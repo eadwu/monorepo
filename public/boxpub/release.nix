a02d0054a628b9466a8dd0bd61c685c4153697aa
Edmund Wu <fangkazuto@gmail.com>
2019-03-10 16:46:51 -0400

    cabal: include missing dependencies

--- public/boxpub/release.nix
+++ public/boxpub/release.nix
@@ -1,4 +1,6 @@
-{ mkDerivation, base, fs, optparse-applicative, stdenv }:
+{ mkDerivation, base, fs, http-client, http-client-tls
+, http-conduit, optparse-applicative, stdenv
+}:
 mkDerivation {
   pname = "boxpub";
   version = "0.1.0.0";
@@ -7,7 +9,9 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [ base ];
   librarySystemDepends = [ fs ];
-  executableHaskellDepends = [ base optparse-applicative ];
+  executableHaskellDepends = [
+    base http-client http-client-tls http-conduit optparse-applicative
+  ];
   executableSystemDepends = [ fs ];
   homepage = "https://git.sr.ht/~eadwu/boxpub";
   description = "boxnovel epub generator";
