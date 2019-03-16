4b73805180f0c0a9633143d9b0831997272f4c47
Edmund Wu <fangkazuto@gmail.com>
2019-03-15 21:54:20 -0400

    boxpub: use scalpel for web scraping

--- src/Boxpub/Client/Env.hs
+++ src/Boxpub/Client/Env.hs
@@ -4,20 +4,14 @@ module Boxpub.Client.Env
 , boxpubVersion ) where
   import Data.Version ( showVersion )
   import Boxpub.Client.Parser ( BoxpubOptions(..) )
-  import Network.HTTP.Client ( Manager )
-  import Network.HTTP.Client.TLS ( newTlsManagerWith, tlsManagerSettings )
   import qualified Paths_boxpub as Boxpub
 
   data Env = Env
-    { options :: BoxpubOptions
-    , manager :: Manager }
+    { options :: BoxpubOptions }
 
   mkEnv :: BoxpubOptions -> IO Env
-  mkEnv options = do
-    manager <- newTlsManagerWith tlsManagerSettings
-    return Env
-      { options = options
-      , manager = manager }
+  mkEnv options = return Env
+    { options = options }
 
   boxpubVersion :: String
   boxpubVersion = "boxpub " ++ showVersion Boxpub.version
