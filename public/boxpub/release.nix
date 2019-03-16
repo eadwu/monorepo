9ea17f4dba6bb488e2b369524dd3903039441e61
Edmund Wu <fangkazuto@gmail.com>
2019-03-15 21:54:20 -0400

    boxpub: use scalpel for web scraping

--- public/boxpub/release.nix
+++ public/boxpub/release.nix
@@ -1,6 +1,4 @@
-{ mkDerivation, base, fs, http-client, http-client-tls
-, http-conduit, optparse-applicative, stdenv
-}:
+{ mkDerivation, base, fs, optparse-applicative, scalpel, stdenv }:
 mkDerivation {
   pname = "boxpub";
   version = "0.1.0.0";
@@ -9,9 +7,7 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [ base ];
   librarySystemDepends = [ fs ];
-  executableHaskellDepends = [
-    base http-client http-client-tls http-conduit optparse-applicative
-  ];
+  executableHaskellDepends = [ base optparse-applicative scalpel ];
   executableSystemDepends = [ fs ];
   homepage = "https://git.sr.ht/~eadwu/boxpub";
   description = "boxnovel epub generator";
