09035a0a3bab5d59a6b06fdb57e11e9280b4cdf1
Edmund Wu <fangkazuto@gmail.com>
2019-03-17 14:19:23 -0400

    nix-shell: update dependencies
    Custom ghc with build dependencies
    Include haskell language server

--- shell.nix
+++ shell.nix
@@ -2,15 +2,23 @@
 
 with pkgs;
 
-stdenv.mkDerivation {
+let
+  hies = (import (builtins.fetchTarball {
+    url = "https://github.com/domenkozar/hie-nix/tarball/master";
+  }) { }).hies;
+
+  boxpub = (import ./default.nix { }).boxpub;
+  ghc = haskell.packages."${compiler}".ghcWithPackages (ps: boxpub.propagatedBuildInputs);
+in stdenv.mkDerivation {
   name = "boxpub";
 
   buildInputs = [
     cargo
     rustc
 
+    ghc
+    hies
     cabal2nix
-    haskell.packages."${compiler}".ghc
     haskell.packages."${compiler}".hlint
     haskell.packages."${compiler}".cabal-install
   ];
