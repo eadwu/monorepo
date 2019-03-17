3ff4603f79ff2c0679e73585adbbafa17d34d693
Edmund Wu <fangkazuto@gmail.com>
2019-03-17 14:18:21 -0400

    boxpub: die on invalid args

--- src/Boxpub/Client.hs
+++ src/Boxpub/Client.hs
@@ -4,13 +4,17 @@ module Boxpub.Client
   import Boxpub.Client.FileSystem ( printString )
   import Boxpub.Client.Parser ( BoxpubOptions(..), getOptions )
   import Foreign.C.String ( newCString )
+  import Data.Maybe ( isJust, fromJust )
+  import System.Exit ( die )
 
   main :: IO ()
   main = do
     rawOptions <- getOptions
     if version rawOptions
       then putStrLn boxpubVersion
-      else do
-        env <- mkEnv rawOptions
-        str <- newCString "Hello World\0"
-        printString str
+      else if not $ isJust $ novel rawOptions
+        then die "Invalid argument for <NOVEL> receieved"
+        else do
+          env <- mkEnv rawOptions
+          str <- newCString "Hello World\0"
+          printString str
