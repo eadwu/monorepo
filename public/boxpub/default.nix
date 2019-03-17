7558fa8aa47c4d6d529945112ef564d6364721c0
Edmund Wu <fangkazuto@gmail.com>
2019-03-17 10:10:47 -0400

    boxpub: use latest scalpel

--- public/boxpub/default.nix
+++ public/boxpub/default.nix
@@ -12,7 +12,10 @@ rec {
     cargoSha256 = "0jacm96l1gw9nxwavqi1x4669cg6lzy9hr18zjpwlcyb3qkw9z7f";
   };
 
-  boxpub = haskell.packages."${compiler}".callPackage ./release.nix {
+  boxpub = with haskell.packages."${compiler}"; callPackage ./release.nix {
     inherit fs;
+    scalpel = scalpel_0_6_0.override {
+      scalpel-core = scalpel-core_0_6_0;
+    };
   };
 }
