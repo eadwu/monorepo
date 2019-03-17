2b9770d2d68778d5b4ce44ff737e96e086bc2ff4
Edmund Wu <fangkazuto@gmail.com>
2019-03-17 10:11:33 -0400

    boxpub: include dependencies for https

--- release.nix
+++ release.nix
@@ -1,4 +1,6 @@
-{ mkDerivation, base, fs, optparse-applicative, scalpel, stdenv }:
+{ mkDerivation, base, fs, http-client, http-client-tls
+, optparse-applicative, scalpel, stdenv
+}:
 mkDerivation {
   pname = "boxpub";
   version = "0.1.0.0";
@@ -7,7 +9,9 @@ mkDerivation {
   isExecutable = true;
   libraryHaskellDepends = [ base ];
   librarySystemDepends = [ fs ];
-  executableHaskellDepends = [ base optparse-applicative scalpel ];
+  executableHaskellDepends = [
+    base http-client http-client-tls optparse-applicative scalpel
+  ];
   executableSystemDepends = [ fs ];
   homepage = "https://git.sr.ht/~eadwu/boxpub";
   description = "boxnovel epub generator";
