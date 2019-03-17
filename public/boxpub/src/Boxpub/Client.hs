fed4b026a54ec4686deac739cb4357ab93653637
Edmund Wu <fangkazuto@gmail.com>
2019-03-17 17:48:38 -0400

    Boxpub.Client: use Boxpub.Client.Provider's main

--- public/boxpub/src/Boxpub/Client.hs
+++ public/boxpub/src/Boxpub/Client.hs
@@ -1,11 +1,10 @@
 module Boxpub.Client
 ( main ) where
   import Boxpub.Client.Env ( mkEnv, boxpubVersion )
-  import Boxpub.Client.FileSystem ( printString )
   import Boxpub.Client.Parser ( BoxpubOptions(..), getOptions )
-  import Foreign.C.String ( newCString )
   import Data.Maybe ( isJust, fromJust )
   import System.Exit ( die )
+  import qualified Boxpub.Client.Provider as BP
 
   main :: IO ()
   main = do
@@ -16,5 +15,4 @@ module Boxpub.Client
         then die "Invalid argument for <NOVEL> receieved"
         else do
           env <- mkEnv rawOptions
-          str <- newCString "Hello World\0"
-          printString str
+          BP.main env
