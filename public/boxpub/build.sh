00e21ac3bc1f2fe3aa43b42073b4b1617edea1fc
Edmund Wu <fangkazuto@gmail.com>
2019-03-21 22:05:36 -0400

    build.sh: try to make os-agnostic

--- public/boxpub/build.sh
+++ public/boxpub/build.sh
@@ -1,22 +1,15 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash
+#! /usr/bin/env bash
 
 function fail() {
   echo "$1" >&2
   exit 1
 }
 
-CABAL=$(command -v cabal)
 CARGO=$(command -v cargo)
-
-if [ -z "$CABAL" ]; then
-  fail "cabal executable not found"
-fi
 
 if [ -z "$CARGO" ]; then
   fail "cargo executable not found"
 fi
 
-$CARGO build --lib --release
 $CABAL new-update
 $CABAL new-build
