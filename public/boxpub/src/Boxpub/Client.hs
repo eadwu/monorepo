4507dd2a1fdfc322f596aa75cef832437f398b64
Edmund Wu <fangkazuto@gmail.com>
2019-03-23 18:18:28 -0400

    treewide: use Text for performance only
    Or in this case, when the string can be absurdly long.

--- public/boxpub/src/Boxpub/Client.hs
+++ public/boxpub/src/Boxpub/Client.hs
@@ -1,10 +1,8 @@
 module Boxpub.Client
 ( main ) where
-  import Prelude hiding ( putStrLn )
   import Boxpub.Client.Env ( mkEnv, boxpubVersion )
   import Boxpub.Client.Parser ( BoxpubOptions(..), getOptions )
   import Data.Maybe ( isNothing )
-  import Data.Text.IO ( putStrLn )
   import System.Exit ( die )
 
   main :: IO ()
