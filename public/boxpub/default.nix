b2c05f5d9479e506bedcf83a89f01de73cc3be49
Edmund Wu <fangkazuto@gmail.com>
2019-03-24 15:18:40 -0400

    boxpub: add pin for 1.0.0.0

--- public/boxpub/default.nix
+++ public/boxpub/default.nix
@@ -10,4 +10,14 @@ with pkgs;
       scalpel-core = "0.6.0";
     };
   };
+
+  boxpub-1_x = with haskell.packages."${compiler}"; callCabal2nix "boxpub-1_x" (fetchgit {
+    url = ./.;
+    rev = "1.0.0.0";
+    sha256 = "0jiw9chailika68q84f06agry4dhfxqsrm589azla8shxqwc961s";
+  }) {
+    scalpel = scalpel_0_6_0.override {
+      scalpel-core = scalpel-core_0_6_0;
+    };
+  };
 }
