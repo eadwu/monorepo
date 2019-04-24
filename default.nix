a9a97201905f61ed59c55279e8855bce8fc8eaee
Edmund Wu <fangkazuto@gmail.com>
2019-04-23 22:18:31 -0400

    boxpub-1_x: 1.1.1.0 -> 1.1.2.0

--- default.nix
+++ default.nix
@@ -13,8 +13,8 @@ with nixpkgs.pkgs;
 
   boxpub-1_x = with haskell.packages."${compiler}"; callCabal2nix "boxpub-1_x" (fetchgit {
     url = ./.;
-    rev = "1.1.1.0";
-    sha256 = "0l17y851vjnbyggg66bh5vjrr0fpr8vckn9q1asjzzakria7nqcf";
+    rev = "1.1.2.0";
+    sha256 = "0cfyrnqff50s2w5s6b4fqaf3hlh94npxyndc776j72wk1d4spc8q";
   }) {
     scalpel = scalpel_0_6_0.override {
       scalpel-core = scalpel-core_0_6_0;
