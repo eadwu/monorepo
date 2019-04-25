014b47d147dca770790e90e8242a282149aa162b
Edmund Wu <fangkazuto@gmail.com>
2019-04-25 11:26:20 -0400

    nix: patch pandoc for EPUB 3.2 ttf specification

--- default.nix
+++ default.nix
@@ -2,8 +2,20 @@
 
 with nixpkgs.pkgs;
 
-{
-  boxpub = haskell.packages."${compiler}".developPackage {
+let
+  ghcPackages = haskell.packages."${compiler}".override {
+    overrides = self: super: {
+      pandoc = super.pandoc.overrideAttrs (oldAttrs: {
+        prePatch = (oldAttrs.prePatch or "") + ''
+          # Conform to EPUB 3.2 ttf font specifications
+          # See https://w3c.github.io/publ-epub-revision/epub32/spec/epub-spec.html#sec-cmt-supported
+          sed -i 's@\(("ttf","\)application/x-font-truetype\(")\)@\1application/font-sfnt\2@' src/Text/Pandoc/MIME.hs
+        '';
+      });
+    };
+  };
+in with ghcPackages; {
+  boxpub = developPackage {
     root = ./.;
     source-overrides = {
       scalpel = "0.6.0";
@@ -11,7 +23,7 @@ with nixpkgs.pkgs;
     };
   };
 
-  boxpub-1_x = with haskell.packages."${compiler}"; callCabal2nix "boxpub-1_x" (fetchgit {
+  boxpub-1_x = callCabal2nix "boxpub-1_x" (fetchgit {
     url = ./.;
     rev = "1.2.1.0";
     sha256 = "1x6379s2cd8crgibwhybf17j3i47h9lvmp62zh91ginw6661q68i";
