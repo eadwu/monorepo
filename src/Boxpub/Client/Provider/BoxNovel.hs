64c910de088729c6326929d86267e24cae14079f
Edmund Wu <fangkazuto@gmail.com>
2019-04-25 15:51:17 -0400

    Boxpub.Client.Provider.BoxNovel: purge BoxNovelEnv

--- src/Boxpub/Client/Provider/BoxNovel.hs
+++ src/Boxpub/Client/Provider/BoxNovel.hs
@@ -1,18 +1,8 @@
 module Boxpub.Client.Provider.BoxNovel
-( Paths(..), BoxNovelEnv(..)
-, mkEnv
-, getRootPath, getNovelPath, getChapterPath
+( novelPath
 , novelTitle, novelAuthor, coverImage, chapterList, chapterName, chapterContents ) where
   import Data.Text as T
   import Text.HTML.Scalpel
-
-  data Paths = Paths
-    { root :: Text
-    , novel :: Text
-    , chapter :: Text }
-
-  data BoxNovelEnv = BoxNovelEnv
-    { paths :: Paths }
 
   protocol :: Text
   protocol = "https"
@@ -20,24 +10,8 @@ module Boxpub.Client.Provider.BoxNovel
   domain :: Text
   domain = "boxnovel.com"
 
-  mkEnv :: IO BoxNovelEnv
-  mkEnv = return BoxNovelEnv
-    { paths = paths }
-    where
-      novel = "/novel/%s"
-      paths = Paths
-        { root = T.concat [ protocol, "://", domain ]
-        , novel = novel
-        , chapter = T.concat [ novel, "/chapter-%d" ] }
-
-  getRootPath :: BoxNovelEnv -> Text
-  getRootPath = root . paths
-
-  getNovelPath :: BoxNovelEnv -> Text
-  getNovelPath = novel . paths
-
-  getChapterPath :: BoxNovelEnv -> Text
-  getChapterPath = chapter . paths
+  novelPath :: Text
+  novelPath = T.concat [ protocol, "://", domain, "/novel/%s" ]
 
   novelTitle :: Scraper Text Text
   novelTitle = text $ "div" @: [ hasClass "post-title" ]
