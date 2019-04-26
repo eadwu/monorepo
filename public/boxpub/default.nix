4844e9e13d049b1d81fa4d7a37c6e77d8049d4b9
Edmund Wu <fangkazuto@gmail.com>
2019-04-26 13:10:58 -0400

    nix: override scalpel within ghc expression

--- public/boxpub/default.nix
+++ public/boxpub/default.nix
@@ -3,8 +3,11 @@
 with nixpkgs.pkgs;
 
 let
-  ghcPackages = haskell.packages."${compiler}".override {
+  ghc = haskell.packages."${compiler}".override {
     overrides = self: super: {
+      scalpel = self.callHackage "scalpel" "0.6.0" { };
+      scalpel-core = self.callHackage "scalpel-core" "0.6.0" { };
+
       pandoc = super.pandoc.overrideAttrs (oldAttrs: {
         prePatch = (oldAttrs.prePatch or "") + ''
           # Conform to EPUB 3.2 ttf font specifications
@@ -14,22 +17,15 @@ let
       });
     };
   };
-in with ghcPackages; {
+in with ghc; {
   boxpub = developPackage {
     root = ./.;
-    source-overrides = {
-      scalpel = "0.6.0";
-      scalpel-core = "0.6.0";
-    };
+    source-overrides = { };
   };
 
   boxpub-1_x = callCabal2nix "boxpub-1_x" (fetchgit {
     url = ./.;
     rev = "1.2.2.0";
     sha256 = "0qflibwbzav7r19n51vya1zi238jz5xr0zf3pz1hhkl7bapw7cvi";
-  }) {
-    scalpel = scalpel_0_6_0.override {
-      scalpel-core = scalpel-core_0_6_0;
-    };
-  };
+  }) { };
 }
