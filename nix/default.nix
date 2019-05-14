66491b37432453d79f111e5521e35a2b2d3392da
Edmund Wu <fangkazuto@gmail.com>
2019-05-13 20:45:10 -0400

    nix: pin haskell.nix and consistent nixpkgs

--- nix/default.nix
+++ nix/default.nix
@@ -4,8 +4,11 @@ let
   haskell_nix = pkgs.stdenv.mkDerivation {
     name = "haskell_nix";
 
-    src = builtins.fetchTarball {
-      url = "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz";
+    src = pkgs.fetchFromGitHub {
+      owner = "input-output-hk";
+      repo = "haskell.nix";
+      rev = "ff3b3162df1b957a1866af1bbd6d98eb36da0afa";
+      sha256 = "1fcqkw82x9xfhr66ac30fb7r1n4v80xpq4jfs3f800sy4fmpmq9b";
     };
 
     patches = [
@@ -20,7 +23,10 @@ let
     '';
   };
 
-  haskell = import haskell_nix { inherit pkgs; };
+  haskell = import haskell_nix {
+    inherit pkgs;
+    nixpkgs = "<nixpkgs>";
+  };
 
   pkgSet = haskell.mkCabalProjectPkgSet {
     plan-pkgs = import ./pkgs.nix;
