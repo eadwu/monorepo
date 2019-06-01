fa232a530a21dd9ff4090ce02b52d3366fe3b846
Edmund Wu <fangkazuto@gmail.com>
2019-06-01 17:57:12 -0400

    Boxpub.Client: document main internally

--- src/Boxpub/Client.hs
+++ src/Boxpub/Client.hs
@@ -9,11 +9,16 @@ module Boxpub.Client
 
   main :: IO ()
   main = do
+    -- Get options as a "readable" data structure
     rawOptions <- getOptions
     if version rawOptions
+      -- If `-v` or `--version` is given output the version and exit
       then T.putStrLn boxpubVersion
+      -- Otherwise make sure the mandatory parameter is given
       else if isNothing $ novel rawOptions
+        -- Exit with an error and nonzero exit code
         then die "Invalid argument for <NOVEL> receieved"
+        -- Proceed with the intended operation of the program
         else do
           env <- mkEnv rawOptions
           E.main env
