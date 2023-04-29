5aa1f8fe9b4cd6fd17deade25ddc5d70483e5590
Edmund Wu <fangkazuto@gmail.com>
2023-04-28 23:33:34 -0700

    primitives/ops: define elementary arithmetic operations

--- src/dtensor/primitives/ops.rs
+++ src/dtensor/primitives/ops.rs
@@ -1,2 +1,21 @@
-mod addition;
-pub use self::addition::add;
+pub mod builders;
+
+use builders::{*};
+use crate::dtensor::primitives::{Tensor};
+
+// Elementary Arithmetic
+pub async fn add<'a>(a: &Tensor<'a>, b: &Tensor<'a>) -> Tensor<'a> {
+  elementary_arithmetic::elementary_arithmetic_builder(a, b, "add", "+").await
+}
+
+pub async fn subtract<'a>(a: &Tensor<'a>, b: &Tensor<'a>) -> Tensor<'a> {
+  elementary_arithmetic::elementary_arithmetic_builder(a, b, "subtract", "-").await
+}
+
+pub async fn multiply<'a>(a: &Tensor<'a>, b: &Tensor<'a>) -> Tensor<'a> {
+  elementary_arithmetic::elementary_arithmetic_builder(a, b, "multiply", "*").await
+}
+
+pub async fn divide<'a>(a: &Tensor<'a>, b: &Tensor<'a>) -> Tensor<'a> {
+  elementary_arithmetic::elementary_arithmetic_builder(a, b, "subtract", "/").await
+}
--- src/dtensor/primitives/ops/builders/elementary_arithmetic.rs
+++ src/dtensor/primitives/ops/builders/elementary_arithmetic.rs
@@ -1,32 +1,50 @@
-use crate::dtensor::primitives::Tensor;
+use crate::dtensor::{self, primitives::Tensor};
 use wgpu;
 
-const ENTRY_POINT: &str = "add";
 const WORKGROUP_SIZE: usize = 64;
 
 // Functional implementation
-pub async fn add<'add>(a: &Tensor<'add>, b: &Tensor<'add>) -> Tensor<'add> {
+pub async fn elementary_arithmetic_builder<'op>(
+    a: &Tensor<'op>,
+    b: &Tensor<'op>,
+    entry_point: &str,
+    op: &str,
+) -> Tensor<'op> {
     if !std::ptr::eq(a.device(), b.device()) {
         panic!("Can't perform operations on Tensors on different devices");
     }
 
     let wgpu_device = a.device();
-    let (device, queue) = wgpu_device;
+    let (device, _) = wgpu_device;
 
     let output_shape = compute_output_shape(a, b);
-    let sum = Tensor::of_shape(output_shape, wgpu_device).await;
+    let result = Tensor::of_shape(output_shape, wgpu_device).await;
 
-    let shader_source = generate_wgsl_shader(a, b, &sum);
+    let shader_source = generate_wgsl_shader(a, b, &result, entry_point, op);
     let compiled_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
         source: wgpu::ShaderSource::Wgsl(shader_source.into()),
     });
 
+    elementary_arithmetic_op(a, b, &result, wgpu_device, &compiled_shader, entry_point);
+    result
+}
+
+fn elementary_arithmetic_op<'op>(
+    a: &Tensor<'op>,
+    b: &Tensor<'op>,
+    result: &Tensor<'op>,
+    wgpu_device: &dtensor::WgpuDevice,
+    compiled_shader: &wgpu::ShaderModule,
+    entry_point: &str,
+) {
+    let (device, queue) = wgpu_device;
+
     let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
         label: None,
         layout: None,
-        module: &compiled_shader,
-        entry_point: ENTRY_POINT,
+        module: compiled_shader,
+        entry_point: entry_point,
     });
 
     let a_bind_group_layout = pipeline.get_bind_group_layout(0);
@@ -35,8 +53,8 @@ pub async fn add<'add>(a: &Tensor<'add>, b: &Tensor<'add>) -> Tensor<'add> {
     let b_bind_group_layout = pipeline.get_bind_group_layout(1);
     let b_bind_group = tensor_as_input(b, &b_bind_group_layout, device);
 
-    let sum_bind_group_layout = pipeline.get_bind_group_layout(2);
-    let sum_bind_group = tensor_as_input(&sum, &sum_bind_group_layout, device);
+    let result_bind_group_layout = pipeline.get_bind_group_layout(2);
+    let result_bind_group = tensor_as_input(result, &result_bind_group_layout, device);
 
     let mut encoder =
         device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
@@ -47,13 +65,11 @@ pub async fn add<'add>(a: &Tensor<'add>, b: &Tensor<'add>) -> Tensor<'add> {
 
         cpass.set_bind_group(0, &a_bind_group, &[]);
         cpass.set_bind_group(1, &b_bind_group, &[]);
-        cpass.set_bind_group(2, &sum_bind_group, &[]);
+        cpass.set_bind_group(2, &result_bind_group, &[]);
 
-        cpass.dispatch_workgroups(sum.len() as u32, 1, 1);
+        cpass.dispatch_workgroups(result.len() as u32, 1, 1);
     }
     queue.submit(Some(encoder.finish()));
-
-    sum
 }
 
 // Utility functions
@@ -80,30 +96,38 @@ fn tensor_as_input(
     })
 }
 
-fn generate_wgsl_shader(_a: &Tensor, _b: &Tensor, sum: &Tensor) -> String {
+fn generate_wgsl_shader(
+    _a: &Tensor,
+    _b: &Tensor,
+    result: &Tensor,
+    entry_point: &str,
+    op: &str,
+) -> String {
     "".to_string()
         + "
 @group(0) @binding(0) var<storage, read> left: array<f32>;
 
 @group(1) @binding(0) var<storage, read> right: array<f32>;
 
-@group(2) @binding(0) var<storage, read_write> sum: array<f32>;
-const sum_size: u32 = "
-        + &sum.len().to_string()
+@group(2) @binding(0) var<storage, read_write> result: array<f32>;
+const result_size: u32 = "
+        + &result.len().to_string()
         + "u;
 
 @compute @workgroup_size("
         + &WORKGROUP_SIZE.to_string()
         + ", 1, 1)
-fn " + ENTRY_POINT
+fn " + entry_point
         + "(@builtin(global_invocation_id) global_id: vec3u) {
   // Guard against out-of-bounds work group sizes
-  if (global_id.x >= sum_size) {
+  if (global_id.x >= result_size) {
     return;
   }
 
   let index = global_id.x;
-  sum[index] = left[index] + right[index];
+  result[index] = left[index] "
+        + op
+        + " right[index];
 }
 "
 }
