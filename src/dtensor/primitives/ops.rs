8e1fdf834295b58afa6aa404c3ea918e235f391b
Edmund Wu <fangkazuto@gmail.com>
2023-04-30 13:30:28 -0400

    primitives/ops: format code

--- src/dtensor/primitives/ops.rs
+++ src/dtensor/primitives/ops.rs
@@ -1,21 +1,21 @@
 pub mod builders;
 
-use builders::{*};
-use crate::dtensor::primitives::{Tensor};
+use crate::dtensor::primitives::Tensor;
+use builders::*;
 
 // Elementary Arithmetic
 pub async fn add<'a>(a: &Tensor<'a>, b: &Tensor<'a>) -> Tensor<'a> {
-  elementary_arithmetic::elementary_arithmetic_builder(a, b, "add", "+").await
+    elementary_arithmetic::elementary_arithmetic_builder(a, b, "add", "+").await
 }
 
 pub async fn subtract<'a>(a: &Tensor<'a>, b: &Tensor<'a>) -> Tensor<'a> {
-  elementary_arithmetic::elementary_arithmetic_builder(a, b, "subtract", "-").await
+    elementary_arithmetic::elementary_arithmetic_builder(a, b, "subtract", "-").await
 }
 
 pub async fn multiply<'a>(a: &Tensor<'a>, b: &Tensor<'a>) -> Tensor<'a> {
-  elementary_arithmetic::elementary_arithmetic_builder(a, b, "multiply", "*").await
+    elementary_arithmetic::elementary_arithmetic_builder(a, b, "multiply", "*").await
 }
 
 pub async fn divide<'a>(a: &Tensor<'a>, b: &Tensor<'a>) -> Tensor<'a> {
-  elementary_arithmetic::elementary_arithmetic_builder(a, b, "subtract", "/").await
+    elementary_arithmetic::elementary_arithmetic_builder(a, b, "subtract", "/").await
 }
