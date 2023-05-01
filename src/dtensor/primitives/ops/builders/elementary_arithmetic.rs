2a7aa0ded9afa47cc8b1076b97ca6a67185cd67d
Edmund Wu <fangkazuto@gmail.com>
2023-05-01 16:53:03 -0400

    primitives/ops/builders: refactor implementations

--- src/dtensor/primitives/ops/builders/elementary_arithmetic.rs
+++ src/dtensor/primitives/ops/builders/elementary_arithmetic.rs
@@ -1,4 +1,4 @@
-use crate::dtensor::{self, primitives::Tensor};
+use crate::dtensor::{self, primitives::ops::builders, primitives::Tensor};
 use wgpu;
 
 const WORKGROUP_SIZE: usize = 64;
@@ -14,62 +14,42 @@ pub async fn elementary_arithmetic_builder<'op>(
         panic!("Can't perform operations on Tensors on different devices");
     }
 
-    let wgpu_device = a.device();
+    // Strided Tensors are unlikely to be friendly to cache
+    let contiguous_a = a.as_contiguous().await;
+    let contiguous_b = b.as_contiguous().await;
+
+    let wgpu_device = contiguous_a.device();
     let (device, _) = wgpu_device;
 
-    let output_shape = compute_output_shape(a, b);
+    let output_shape = compute_output_shape(&contiguous_a, &contiguous_b);
     let result = Tensor::of_shape(&output_shape, wgpu_device).await;
 
-    let shader_source = generate_wgsl_shader(a, b, &result, entry_point, op);
+    let pipeline_descriptor = builders::TensorOpDescriptor {
+        inputs: &[
+            builders::TensorDescriptor {
+                name: "left",
+                tensor: &contiguous_a,
+            },
+            builders::TensorDescriptor {
+                name: "right",
+                tensor: &contiguous_b,
+            },
+        ],
+        output: builders::TensorDescriptor {
+            name: "result",
+            tensor: &result,
+        },
+        entry_point: entry_point,
+    };
+
+    let shader_source = generate_wgsl_shader(&pipeline_descriptor, op);
     let compiled_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
         source: wgpu::ShaderSource::Wgsl(shader_source.into()),
     });
 
-    elementary_arithmetic_op(a, b, &result, wgpu_device, &compiled_shader, entry_point);
+    builders::build_op_pipeline(&pipeline_descriptor, &compiled_shader, wgpu_device);
     result
-}
-
-fn elementary_arithmetic_op<'op>(
-    a: &Tensor<'op>,
-    b: &Tensor<'op>,
-    result: &Tensor<'op>,
-    wgpu_device: &dtensor::WgpuDevice,
-    compiled_shader: &wgpu::ShaderModule,
-    entry_point: &str,
-) {
-    let (device, queue) = wgpu_device;
-
-    let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-        label: None,
-        layout: None,
-        module: compiled_shader,
-        entry_point: entry_point,
-    });
-
-    let a_bind_group_layout = pipeline.get_bind_group_layout(0);
-    let a_bind_group = tensor_as_input(a, &a_bind_group_layout, device);
-
-    let b_bind_group_layout = pipeline.get_bind_group_layout(1);
-    let b_bind_group = tensor_as_input(b, &b_bind_group_layout, device);
-
-    let result_bind_group_layout = pipeline.get_bind_group_layout(2);
-    let result_bind_group = tensor_as_input(result, &result_bind_group_layout, device);
-
-    let mut encoder =
-        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-    {
-        let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
-
-        cpass.set_pipeline(&pipeline);
-
-        cpass.set_bind_group(0, &a_bind_group, &[]);
-        cpass.set_bind_group(1, &b_bind_group, &[]);
-        cpass.set_bind_group(2, &result_bind_group, &[]);
-
-        cpass.dispatch_workgroups(result.len() as u32, 1, 1);
-    }
-    queue.submit(Some(encoder.finish()));
 }
 
 // Utility functions
@@ -81,53 +61,31 @@ fn compute_output_shape(a: &Tensor, b: &Tensor) -> Vec<usize> {
         .collect()
 }
 
-fn tensor_as_input(
-    tensor: &Tensor,
-    bind_group_layout: &wgpu::BindGroupLayout,
-    device: &wgpu::Device,
-) -> wgpu::BindGroup {
-    device.create_bind_group(&wgpu::BindGroupDescriptor {
-        label: None,
-        layout: bind_group_layout,
-        entries: &[wgpu::BindGroupEntry {
-            binding: 0,
-            resource: tensor.data.as_entire_binding(),
-        }],
-    })
-}
+fn generate_wgsl_shader(pipeline_descriptor: &builders::TensorOpDescriptor, op: &str) -> String {
+    format!(
+        "
+{shader_interface}
 
-fn generate_wgsl_shader(
-    _a: &Tensor,
-    _b: &Tensor,
-    result: &Tensor,
-    entry_point: &str,
-    op: &str,
-) -> String {
-    "".to_string()
-        + "
-@group(0) @binding(0) var<storage, read> left: array<f32>;
-
-@group(1) @binding(0) var<storage, read> right: array<f32>;
-
-@group(2) @binding(0) var<storage, read_write> result: array<f32>;
-const result_size: u32 = "
-        + &result.len().to_string()
-        + "u;
-
-@compute @workgroup_size("
-        + &WORKGROUP_SIZE.to_string()
-        + ", 1, 1)
-fn " + entry_point
-        + "(@builtin(global_invocation_id) global_id: vec3u) {
+@compute @workgroup_size({workgroup_size}, 1, 1)
+fn {entry_point}(@builtin(global_invocation_id) global_id: vec3u) {{
   // Guard against out-of-bounds work group sizes
-  if (global_id.x >= result_size) {
+  if (global_id.x >= result_metadata.length) {{
     return;
-  }
+  }}
 
-  let index = global_id.x;
-  result[index] = left[index] "
-        + op
-        + " right[index];
-}
-"
+  {workarounds}
+
+  let index: u32 = global_id.x;
+  result[index] = left[index] {op} right[index];
+}}
+",
+        shader_interface = builders::define_shader_interface(
+            pipeline_descriptor.inputs,
+            &pipeline_descriptor.output
+        ),
+        workgroup_size = WORKGROUP_SIZE,
+        entry_point = pipeline_descriptor.entry_point,
+        workarounds = builders::shader_workaround_1976(pipeline_descriptor),
+        op = op,
+    )
 }
--- src/dtensor/primitives/ops/builders/elementwise_functions.rs
+++ src/dtensor/primitives/ops/builders/elementwise_functions.rs
@@ -1,4 +1,4 @@
-use crate::dtensor::{self, primitives::Tensor};
+use crate::dtensor::{self, primitives::ops::builders, primitives::Tensor};
 use wgpu;
 
 const WORKGROUP_SIZE: usize = 64;
@@ -14,101 +14,60 @@ pub async fn elementwise_function_builder<'op>(
 
     let result = Tensor::of_shape(a.shape(), wgpu_device).await;
 
-    let shader_source = generate_wgsl_shader(a, &result, entry_point, elementwise_function);
+    let pipeline_descriptor = builders::TensorOpDescriptor {
+        inputs: &[builders::TensorDescriptor {
+            name: "input",
+            tensor: a,
+        }],
+        output: builders::TensorDescriptor {
+            name: "result",
+            tensor: &result,
+        },
+        entry_point: entry_point,
+    };
+
+    let shader_source = generate_wgsl_shader(&pipeline_descriptor, elementwise_function);
     let compiled_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
         source: wgpu::ShaderSource::Wgsl(shader_source.into()),
     });
 
-    elementwise_function_op(a, &result, wgpu_device, &compiled_shader, entry_point);
+    builders::build_op_pipeline(&pipeline_descriptor, &compiled_shader, wgpu_device);
     result
 }
 
-fn elementwise_function_op<'op>(
-    input: &Tensor<'op>,
-    result: &Tensor<'op>,
-    wgpu_device: &dtensor::WgpuDevice,
-    compiled_shader: &wgpu::ShaderModule,
-    entry_point: &str,
-) {
-    let (device, queue) = wgpu_device;
-
-    let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-        label: None,
-        layout: None,
-        module: compiled_shader,
-        entry_point: entry_point,
-    });
-
-    let input_bind_group_layout = pipeline.get_bind_group_layout(0);
-    let input_bind_group = tensor_as_input(input, &input_bind_group_layout, device);
-
-    let result_bind_group_layout = pipeline.get_bind_group_layout(1);
-    let result_bind_group = tensor_as_input(result, &result_bind_group_layout, device);
-
-    let mut encoder =
-        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-    {
-        let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
-
-        cpass.set_pipeline(&pipeline);
-
-        cpass.set_bind_group(0, &input_bind_group, &[]);
-        cpass.set_bind_group(1, &result_bind_group, &[]);
-
-        cpass.dispatch_workgroups(result.len() as u32, 1, 1);
-    }
-    queue.submit(Some(encoder.finish()));
-}
-
 // Utility functions
-fn tensor_as_input(
-    tensor: &Tensor,
-    bind_group_layout: &wgpu::BindGroupLayout,
-    device: &wgpu::Device,
-) -> wgpu::BindGroup {
-    device.create_bind_group(&wgpu::BindGroupDescriptor {
-        label: None,
-        layout: bind_group_layout,
-        entries: &[wgpu::BindGroupEntry {
-            binding: 0,
-            resource: tensor.data.as_entire_binding(),
-        }],
-    })
-}
-
 fn generate_wgsl_shader(
-    input: &Tensor,
-    result: &Tensor,
-    entry_point: &str,
+    pipeline_descriptor: &builders::TensorOpDescriptor,
     elementwise_function: String,
 ) -> String {
-  // let literal_elementwise_function = format!(elementwise_function, input = "input[index]");
-  let literal_elementwise_function = elementwise_function.replace("{input}", "input[index]");
+    // let literal_elementwise_function = format!(elementwise_function, input = "input[index]");
+    let literal_elementwise_function = elementwise_function.replace("{input}", "input[index]");
 
-    "".to_string()
-        + "
-@group(0) @binding(0) var<storage, read> input: array<f32>;
+    format!(
+        "
+{shader_interface}
 
-@group(1) @binding(0) var<storage, read_write> result: array<f32>;
-const result_size: u32 = "
-        + &result.len().to_string()
-        + "u;
+@compute @workgroup_size({workgroup_size}, 1, 1)
+fn {entry_point}(@builtin(global_invocation_id) global_id: vec3u) {{
+    // Guard against out-of-bounds work group sizes
+    if (global_id.x >= result_metadata.length) {{
+        return;
+    }}
 
-@compute @workgroup_size("
-        + &WORKGROUP_SIZE.to_string()
-        + ", 1, 1)
-fn " + entry_point
-        + "(@builtin(global_invocation_id) global_id: vec3u) {
-  // Guard against out-of-bounds work group sizes
-  if (global_id.x >= result_size) {
-    return;
-  }
+    {workarounds}
 
-  let index = global_id.x;
-  result[index] = "
-        + &literal_elementwise_function
-        + ";
-}
-"
+    let index: u32 = global_id.x;
+    result[index] = {literal_elementwise_function};
+}}
+",
+        shader_interface = builders::define_shader_interface(
+            pipeline_descriptor.inputs,
+            &pipeline_descriptor.output
+        ),
+        workgroup_size = WORKGROUP_SIZE,
+        entry_point = pipeline_descriptor.entry_point,
+        workarounds = builders::shader_workaround_1976(pipeline_descriptor),
+        literal_elementwise_function = literal_elementwise_function,
+    )
 }
