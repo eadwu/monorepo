e032714ff51f115088926662643b86d6985b5536
Edmund Wu <fangkazuto@gmail.com>
2023-08-11 19:53:16 -0400

    filemanager: implement cached file management

--- public/dtensor/dtensor/public/filemanager/src/lib.rs
+++ public/dtensor/dtensor/public/filemanager/src/lib.rs
@@ -1,14 +1,54 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs::File;
+use std::io;
+use std::path::{Path, PathBuf};
+
+use hashlink::LruCache;
+use memmap2::{Mmap, MmapOptions};
+
+pub struct FileManager {
+    cache: LruCache<PathBuf, File>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl FileManager {
+    pub fn new(cache_size: usize) -> FileManager {
+        FileManager {
+            cache: LruCache::new(cache_size),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn open(&mut self, path: &Path, size: usize, offset: usize) -> io::Result<Mmap> {
+        if !self.cache.contains_key(path) {
+            let file = File::open(&path)?;
+            self.cache.insert(path.to_path_buf(), file);
+        }
+
+        let file = self.cache.get(path).unwrap();
+        let minimum_size = (size + offset) as u64;
+        if file.metadata()?.len() < minimum_size {
+            file.set_len(minimum_size)?;
+        }
+
+        Ok(unsafe { MmapOptions::new().offset(offset as u64).map(file).unwrap() })
+    }
+
+    pub fn create(&mut self, path: &Path, size: usize, offset: usize) -> io::Result<Mmap> {
+        if !self.cache.contains_key(path) {
+            let file = tempfile::tempfile()?;
+            self.cache.insert(path.to_path_buf(), file);
+        }
+
+        self.open(path, size, offset)
+    }
+
+    pub fn create_with_bytes(&mut self, path: &Path, bytes: &[u8]) -> io::Result<Mmap> {
+        let mmap = self.create(path, bytes.len(), 0)?;
+        let mut mmap = mmap.make_mut()?;
+        mmap[..].copy_from_slice(bytes);
+
+        mmap.make_read_only()
+    }
+
+    pub fn close(&mut self, path: &Path) {
+        self.cache.remove(path);
     }
 }
