b3eb00d02b4800d002e91ee111329fd5272eee17
Edmund Wu <fangkazuto@gmail.com>
2023-08-12 01:17:12 -0400

    tensor: initialize library FILE_MANAGER

--- tensor/src/lib.rs
+++ tensor/src/lib.rs
@@ -1,1 +1,10 @@
+use std::sync::Mutex;
+
+use filemanager::FileManager;
+use once_cell::sync::Lazy;
+
 pub mod primitives;
+
+const FILE_CACHE_SIZE: usize = 1024;
+static FILE_MANAGER: Lazy<Mutex<FileManager>> =
+    Lazy::new(|| Mutex::new(FileManager::new(FILE_CACHE_SIZE)));
