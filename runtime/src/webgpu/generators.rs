37999cfb7714749494813d2724ae14539f736a4c
Edmund Wu <fangkazuto@gmail.com>
2023-09-09 12:02:21 -0400

    runtime/webgpu: fix strided computation with offsets

--- runtime/src/webgpu/generators.rs
+++ runtime/src/webgpu/generators.rs
@@ -62,14 +62,23 @@ for (var i = {start_axis_var}; i < {end_axis_var}; i++) {{
     let origin_stride = {origin_metadata_variable}.metadata[{origin_metadata_variable}.contiguous_stride_offset + i];
     let origin_shape = {origin_metadata_variable}.metadata[{origin_metadata_variable}.shape_offset + i];
     let origin_offset = {origin_metadata_variable}.metadata[{origin_metadata_variable}.offset_offset + i];
+    let origin_end_offset = origin_offset + origin_shape;
 
     let mapped_stride = {mapped_metadata_variable}.metadata[{mapped_metadata_variable}.stride_offset + i];
     let mapped_shape = {mapped_metadata_variable}.metadata[{mapped_metadata_variable}.shape_offset + i];
     let mapped_offset = {mapped_metadata_variable}.metadata[{mapped_metadata_variable}.offset_offset + i];
 
     let index_at_dimension = {origin_index_var} / origin_stride;
-    {origin_index_var} -= ((index_at_dimension + origin_offset) % origin_shape) * origin_stride;
-    {mapped_index_var} += ((index_at_dimension + mapped_offset) % mapped_shape) * mapped_stride;
+
+    // Skip if not within bounds of View
+    if index_at_dimension < origin_offset || index_at_dimension >= origin_end_offset {{
+        return;
+    }}
+
+    let origin_index_at_dimension = index_at_dimension;
+    let mapped_index_at_dimension = ((index_at_dimension - origin_offset) % mapped_shape) + mapped_offset;
+    {origin_index_var} -= origin_index_at_dimension * origin_stride;
+    {mapped_index_var} += mapped_index_at_dimension * mapped_stride;
 }}
 ",
         origin_index_var = origin_index_var,
