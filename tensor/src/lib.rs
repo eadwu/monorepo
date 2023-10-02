9246e4dccb07a3aeb5a471d05656822e4ea064d5
Edmund Wu <fangkazuto@gmail.com>
2023-10-01 23:24:56 -0400

    nn: new home of onnx glue code

--- tensor/src/lib.rs
+++ tensor/src/lib.rs
@@ -4,9 +4,6 @@ use filemanager::FileManager;
 use once_cell::sync::Lazy;
 
 pub mod primitives;
-pub mod onnx {
-    include!(concat!(env!("OUT_DIR"), "/onnx.rs"));
-}
 
 const FILE_CACHE_SIZE: usize = 1024;
 static FILE_MANAGER: Lazy<Mutex<FileManager>> =
