ce69c77a52e02e34b99572c4e68a84000a3ba3a3
Edmund Wu <fangkazuto@gmail.com>
2023-11-03 17:04:53 -0400

    filemanager: limited size file cache
    History keeps track of temporary files so that they still exist but are not deleted and at the same time not taking a file descriptor

--- public/dtensor/dtensor/public/filemanager/src/lib.rs
+++ public/dtensor/dtensor/public/filemanager/src/lib.rs
@@ -2,43 +2,46 @@ use std::fs::File;
 use std::io;
 use std::path::{Path, PathBuf};
 
-use hashlink::LinkedHashMap;
+use hashlink::{LinkedHashMap, LruCache};
 use memmap2::{Mmap, MmapOptions};
+use tempfile::{NamedTempFile, TempPath};
 
 pub struct FileManager {
-    cache: LinkedHashMap<PathBuf, File>,
+    cache: LruCache<PathBuf, File>,
+    history: LinkedHashMap<PathBuf, TempPath>,
 }
 
 impl FileManager {
     pub fn new(cache_size: usize) -> FileManager {
         FileManager {
-            cache: LinkedHashMap::with_capacity(cache_size),
+            cache: LruCache::new(cache_size),
+            history: LinkedHashMap::new(),
         }
     }
 
     pub fn open(&mut self, path: &Path, offset: usize) -> io::Result<Mmap> {
         if !self.cache.contains_key(path) {
-            let file = File::open(&path)?;
+            let file_path = if self.history.contains_key(path) {
+                self.history.get(path).unwrap()
+            } else {
+                path
+            };
+
+            let file = File::options().read(true).write(true).open(&file_path)?;
             self.cache.insert(path.to_path_buf(), file);
         }
 
         let file = self.cache.get(path).unwrap();
-        Ok(unsafe {
-            MmapOptions::new()
-                .offset(offset as u64)
-                .map(file)
-                .unwrap()
-        })
+        Ok(unsafe { MmapOptions::new().offset(offset as u64).map(file).unwrap() })
     }
 
     pub fn create(&mut self, path: &Path, size: usize, offset: usize) -> io::Result<Mmap> {
         if !self.cache.contains_key(path) {
-            let file = tempfile::tempfile()?;
-            file.set_len(size as u64)?;
-
-            self.cache.insert(path.to_path_buf(), file);
+            let file = NamedTempFile::new()?;
+            file.as_file().set_len(size as u64)?;
+            self.history
+                .insert(path.to_path_buf(), file.into_temp_path());
         }
-
         self.open(path, offset)
     }
 
@@ -46,11 +49,13 @@ impl FileManager {
         let mmap = self.create(path, bytes.len(), 0)?;
         let mut mmap = mmap.make_mut()?;
         mmap[..].copy_from_slice(bytes);
-
         mmap.make_read_only()
     }
 
     pub fn close(&mut self, path: &Path) {
+        if let Some(temp_path) = self.history.remove(path) {
+            let _ = temp_path.close();
+        };
         self.cache.remove(path);
     }
 }
