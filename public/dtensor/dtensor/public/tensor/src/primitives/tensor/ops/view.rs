01247096b287eed8affd6a148c2ca86cc370f98e
Edmund Wu <fangkazuto@gmail.com>
2023-11-14 23:59:21 -0500

    tensor: make use of TensorViewTracker
    Turns out it just needed to be virtually contiguous for operations as well ...
    in hindsight this is what was intuited all along
    
    No more fixed-point ViewOp evaluations

--- public/dtensor/dtensor/public/tensor/src/primitives/tensor/ops/view.rs
+++ public/dtensor/dtensor/public/tensor/src/primitives/tensor/ops/view.rs
@@ -1,7 +1,5 @@
-use crate::primitives::tensor::Tensor;
+use crate::primitives::tensor::{Tensor, TensorInput};
 use crate::primitives::tensorview::{TensorView, ViewType};
-
-use super::{OperationSpec, TensorInput};
 
 impl Tensor {
     fn view_op(&self, view: &TensorView) -> Tensor {
@@ -10,7 +8,14 @@ impl Tensor {
             TensorInput::no_op(self.clone()),
             self.datatype(),
         )
-        .Identity()
+    }
+
+    fn transparent_view_op(&self, view: &TensorView) -> Tensor {
+        Tensor::new(
+            self.viewtracker().swap_view(view),
+            TensorInput::no_op(self.clone()),
+            self.datatype(),
+        )
     }
 
     pub fn contiguous(&self) -> Tensor {
@@ -27,15 +32,15 @@ impl Tensor {
     }
 
     pub fn squeeze(&self, axis: ViewType) -> Tensor {
-        self.reshape(&self.view().squeeze(axis))
+        self.transparent_view_op(&self.view().squeeze(axis))
     }
 
     pub fn transpose(&self, axes: &[ViewType]) -> Tensor {
-        self.reshape(&self.view().transpose(axes))
+        self.transparent_view_op(&self.view().transpose(axes))
     }
 
     pub fn unsqueeze(&self, axis: ViewType) -> Tensor {
-        self.reshape(&self.view().unsqueeze(axis))
+        self.transparent_view_op(&self.view().unsqueeze(axis))
     }
 
     pub fn reshape(&self, view: &TensorView) -> Tensor {
