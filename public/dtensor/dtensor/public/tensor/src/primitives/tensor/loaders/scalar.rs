7f8b168d3c926f6b1d997e679693f96fd3c16a28
Edmund Wu <fangkazuto@gmail.com>
2023-11-17 16:19:40 -0500

    tensor: byte format is always native endianness
    At least for now...

--- public/dtensor/dtensor/public/tensor/src/primitives/tensor/loaders/scalar.rs
+++ public/dtensor/dtensor/public/tensor/src/primitives/tensor/loaders/scalar.rs
@@ -8,11 +8,11 @@ impl ScalarLoader for &Tensor {
     fn load(self, value: &str) -> Vec<u8> {
         match self.datatype() {
             TensorType::F16 => (value.parse::<f32>().map(half::f16::from_f32).unwrap())
-                .to_le_bytes()
+                .to_ne_bytes()
                 .to_vec(),
-            TensorType::F32 => bytemuck::cast_slice(&[value.parse::<f32>().unwrap()]).to_vec(),
-            TensorType::I32 => bytemuck::cast_slice(&[value.parse::<i32>().unwrap()]).to_vec(),
-            TensorType::U32 => bytemuck::cast_slice(&[value.parse::<u32>().unwrap()]).to_vec(),
+            TensorType::F32 => value.parse::<f32>().unwrap().to_ne_bytes().to_vec(),
+            TensorType::I32 => value.parse::<i32>().unwrap().to_ne_bytes().to_vec(),
+            TensorType::U32 => value.parse::<u32>().unwrap().to_ne_bytes().to_vec(),
         }
     }
 }
