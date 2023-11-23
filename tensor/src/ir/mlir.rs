61cb985762a4ae3b658ce0d7a56f45282842567c
Edmund Wu <fangkazuto@gmail.com>
2023-11-23 01:26:39 -0500

    tensor: add rudimentary IR translation

---
+++ tensor/src/ir/mlir.rs
@@ -0,0 +1,143 @@
+use std::fmt::Display;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::Arc;
+
+use crate::topograph::{GraphDependencies, GraphView, UniqueIdentifier};
+
+mod shader;
+pub use shader::*;
+
+static ID_GENERATOR: AtomicU64 = AtomicU64::new(0);
+
+pub type ShaderIRID = u64;
+
+pub trait ShaderIRBuilder {
+    fn build_shader_ir(&self) -> ShaderIR;
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum ShaderIROp {
+    MagicIndex,
+    ReduceBegin,
+    ReduceEnd,
+    ReduceMagic,
+    Const,
+    Load,
+    Store,
+    Evaluate,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum ShaderIRType {
+    F32,
+    I32,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum ShaderIREvaluation {
+    F32(f32),
+    I32(i32),
+    IDENTITY,
+    EXP2,
+    LOG2,
+    CAST,
+    SIN,
+    SQRT,
+    ABS,
+    FLOOR,
+    CEIL,
+    ADD,
+    SUB,
+    MULTIPLY,
+    DIVIDE,
+    MAX,
+    MOD,
+    EQUAL,
+    LESSTHAN,
+}
+
+impl ShaderIREvaluation {
+    pub fn n_dependencies(&self) -> usize {
+        match self {
+            ShaderIREvaluation::F32(_) => 0,
+            ShaderIREvaluation::I32(_) => 0,
+            ShaderIREvaluation::IDENTITY => 1,
+            ShaderIREvaluation::EXP2 => 1,
+            ShaderIREvaluation::LOG2 => 1,
+            ShaderIREvaluation::CAST => 1,
+            ShaderIREvaluation::SIN => 1,
+            ShaderIREvaluation::SQRT => 1,
+            ShaderIREvaluation::ABS => 1,
+            ShaderIREvaluation::FLOOR => 1,
+            ShaderIREvaluation::CEIL => 1,
+            ShaderIREvaluation::ADD => 2,
+            ShaderIREvaluation::SUB => 2,
+            ShaderIREvaluation::MULTIPLY => 2,
+            ShaderIREvaluation::DIVIDE => 2,
+            ShaderIREvaluation::MAX => 2,
+            ShaderIREvaluation::MOD => 2,
+            ShaderIREvaluation::EQUAL => 2,
+            ShaderIREvaluation::LESSTHAN => 2,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ShaderIR(Arc<ShaderIRInternals>);
+
+#[derive(Clone, Debug)]
+pub struct ShaderIRInternals {
+    id: ShaderIRID,
+    op: ShaderIROp,
+    datatype: ShaderIRType,
+    inputs: Vec<ShaderIR>,
+    evaltype: Option<ShaderIREvaluation>,
+}
+
+impl ShaderIR {
+    pub fn new(
+        op: ShaderIROp,
+        datatype: ShaderIRType,
+        inputs: &[ShaderIR],
+        evaltype: Option<ShaderIREvaluation>,
+    ) -> ShaderIR {
+        let id = ID_GENERATOR.fetch_add(1, Ordering::Relaxed);
+        let inputs = inputs.to_vec();
+        ShaderIR(Arc::new(ShaderIRInternals {
+            id,
+            op,
+            datatype,
+            inputs,
+            evaltype,
+        }))
+    }
+
+    pub fn id(&self) -> ShaderIRID {
+        self.0.id
+    }
+
+    pub fn datatype(&self) -> ShaderIRType {
+        self.0.datatype
+    }
+
+    pub fn inputs(&self) -> &[ShaderIR] {
+        &self.0.inputs[..]
+    }
+}
+
+impl GraphDependencies for ShaderIR {
+    type Dependency = ShaderIR;
+    fn dependencies(&self) -> Vec<Self::Dependency> {
+        self.inputs()
+            .iter()
+            .map(|ir| ir.clone())
+            .collect::<Vec<_>>()
+    }
+}
+
+impl UniqueIdentifier for ShaderIR {
+    type Id = u64;
+    fn id(&self) -> Self::Id {
+        self.id()
+    }
+}
---
+++ tensor/src/ir/mlir/shader.rs
@@ -0,0 +1,487 @@
+use crate::primitives::tensor::*;
+use crate::primitives::tensorview::TensorViewTracker;
+
+use crate::ir::mlir::*;
+
+trait SerializeShaderIR {
+    fn shader_ir(&self, index_ir: &ShaderIR) -> ShaderIR;
+}
+
+trait UnrollShaderIR {
+    fn unroll_ir(&self, metadata: &ShaderIRIndex, ir_type: ShaderIRType) -> Vec<VirtualShaderIR>;
+}
+
+#[derive(Clone, Debug)]
+struct ShaderIROperation {
+    datatype: ShaderIRType,
+    op: ShaderIREvaluation,
+}
+
+#[derive(Clone, Debug)]
+struct ShaderIRIndex {
+    index: ShaderIR,
+    tensor: Tensor,
+}
+
+#[derive(Clone, Debug)]
+struct ShaderIRReduction {
+    reduce_begin: ShaderIR,
+    reduction: ShaderIREvaluation,
+}
+
+#[derive(Clone, Debug)]
+enum VirtualShaderIR {
+    ReduceEnd(ShaderIRReduction),
+    Operator(ShaderIROperation),
+    TranslatedIndex(ShaderIRIndex),
+    Value(ShaderIR),
+}
+
+impl VirtualShaderIR {
+    pub fn op(datatype: ShaderIRType, op: ShaderIREvaluation) -> VirtualShaderIR {
+        VirtualShaderIR::Operator(ShaderIROperation { datatype, op })
+    }
+
+    pub fn index(index: ShaderIR, tensor: Tensor) -> VirtualShaderIR {
+        VirtualShaderIR::TranslatedIndex(ShaderIRIndex { index, tensor })
+    }
+
+    pub fn value(ir: ShaderIR) -> VirtualShaderIR {
+        VirtualShaderIR::Value(ir)
+    }
+
+    pub fn reduce(reduce_begin: ShaderIR, reduction: ShaderIREvaluation) -> VirtualShaderIR {
+        VirtualShaderIR::ReduceEnd(ShaderIRReduction {
+            reduce_begin,
+            reduction,
+        })
+    }
+}
+
+impl ShaderIRBuilder for Tensor {
+    fn build_shader_ir(&self) -> ShaderIR {
+        // Magic initializers
+        let magic_index = ShaderIR::new(ShaderIROp::MagicIndex, ShaderIRType::I32, &[], None);
+        let magic_metadata = VirtualShaderIR::index(magic_index, self.clone());
+
+        // Compute indices using two-level queue, constructed via postfix (reverse-polish notation)
+        let mut inputs = Vec::new();
+        let mut queue = Vec::from([magic_metadata]);
+        while let Some(metadata) = queue.pop() {
+            match metadata {
+                VirtualShaderIR::Operator(operation) => {
+                    let args = (0..operation.op.n_dependencies())
+                        .map(|_| inputs.pop().unwrap())
+                        .collect::<Vec<_>>();
+
+                    let result = ShaderIR::new(
+                        ShaderIROp::Evaluate,
+                        operation.datatype,
+                        &args[..],
+                        Some(operation.op),
+                    );
+                    inputs.push(result);
+                }
+                VirtualShaderIR::TranslatedIndex(ref index_ir) => {
+                    let ir_type = Into::<ShaderIRType>::into(index_ir.tensor.datatype());
+                    let mut unrolled_ir = index_ir.tensor.unroll_ir(&index_ir, ir_type);
+                    queue.append(&mut unrolled_ir);
+                }
+                VirtualShaderIR::Value(value) => inputs.push(value),
+                VirtualShaderIR::ReduceEnd(reduce) => {
+                    let reduce_value = inputs.pop().unwrap();
+                    let result = ShaderIR::new(
+                        ShaderIROp::ReduceEnd,
+                        reduce_value.datatype(),
+                        &[reduce.reduce_begin, reduce_value],
+                        Some(reduce.reduction),
+                    );
+
+                    inputs.push(result);
+                }
+            }
+        }
+
+        assert!(inputs.len() == 1);
+        let final_index = inputs.pop().unwrap();
+        ShaderIR::new(
+            ShaderIROp::Store,
+            Into::<ShaderIRType>::into(self.datatype()),
+            &[final_index],
+            Some(ShaderIREvaluation::I32(self.id() as i32)),
+        )
+    }
+}
+
+impl Into<ShaderIRType> for TensorType {
+    fn into(self) -> ShaderIRType {
+        match &self {
+            TensorType::F32 => ShaderIRType::F32,
+            TensorType::I32 => ShaderIRType::I32,
+            _ => panic!("{:?} is unsupported as ShaderIRType", self),
+        }
+    }
+}
+
+impl Into<ShaderIREvaluation> for UnaryType {
+    fn into(self) -> ShaderIREvaluation {
+        match &self {
+            UnaryType::ABS => ShaderIREvaluation::ABS,
+            UnaryType::CAST => ShaderIREvaluation::CAST,
+            UnaryType::CEIL => ShaderIREvaluation::CEIL,
+            UnaryType::EXP2 => ShaderIREvaluation::EXP2,
+            UnaryType::FLOOR => ShaderIREvaluation::FLOOR,
+            UnaryType::IDENTITY => ShaderIREvaluation::IDENTITY,
+            UnaryType::LOG2 => ShaderIREvaluation::LOG2,
+            UnaryType::SIN => ShaderIREvaluation::SIN,
+            UnaryType::SQRT => ShaderIREvaluation::SQRT,
+        }
+    }
+}
+
+impl Into<ShaderIREvaluation> for BinaryType {
+    fn into(self) -> ShaderIREvaluation {
+        match &self {
+            BinaryType::ADD => ShaderIREvaluation::ADD,
+            BinaryType::DIVIDE => ShaderIREvaluation::DIVIDE,
+            BinaryType::EQUAL => ShaderIREvaluation::EQUAL,
+            BinaryType::LESSTHAN => ShaderIREvaluation::LESSTHAN,
+            BinaryType::MAX => ShaderIREvaluation::MAX,
+            BinaryType::MOD => ShaderIREvaluation::MOD,
+            BinaryType::MULTIPLY => ShaderIREvaluation::MULTIPLY,
+            BinaryType::SUB => ShaderIREvaluation::SUB,
+        }
+    }
+}
+
+impl Into<ShaderIREvaluation> for ReduceType {
+    fn into(self) -> ShaderIREvaluation {
+        match &self {
+            ReduceType::MAX => ShaderIREvaluation::MAX,
+            ReduceType::SUM => ShaderIREvaluation::ADD,
+        }
+    }
+}
+
+impl UnrollShaderIR for Tensor {
+    fn unroll_ir(&self, index_ir: &ShaderIRIndex, ir_type: ShaderIRType) -> Vec<VirtualShaderIR> {
+        match self.data() {
+            TensorInput::ExplicitInput(spec) => vec![match spec {
+                InputSpec::Scalar(_) => {
+                    let bytes = self.load();
+                    match ir_type {
+                        ShaderIRType::F32 => VirtualShaderIR::value(ShaderIR::new(
+                            ShaderIROp::Const,
+                            ir_type,
+                            &[],
+                            Some(ShaderIREvaluation::F32(f32::from_le_bytes(
+                                bytes.try_into().unwrap(),
+                            ))),
+                        )),
+                        ShaderIRType::I32 => VirtualShaderIR::value(ShaderIR::new(
+                            ShaderIROp::Const,
+                            ir_type,
+                            &[],
+                            Some(ShaderIREvaluation::I32(i32::from_le_bytes(
+                                bytes.try_into().unwrap(),
+                            ))),
+                        )),
+                    }
+                }
+                InputSpec::Range(spec) => {
+                    let start_ir = ShaderIR::new(
+                        ShaderIROp::Const,
+                        ShaderIRType::I32,
+                        &[],
+                        Some(ShaderIREvaluation::I32(spec.start as i32)),
+                    );
+                    let step_ir = ShaderIR::new(
+                        ShaderIROp::Const,
+                        ShaderIRType::I32,
+                        &[],
+                        Some(ShaderIREvaluation::I32(spec.step as i32)),
+                    );
+
+                    let sequence_ir = ShaderIR::new(
+                        ShaderIROp::Evaluate,
+                        ShaderIRType::I32,
+                        &[step_ir, index_ir.index.clone()],
+                        Some(ShaderIREvaluation::MULTIPLY),
+                    );
+                    let offset_ir = ShaderIR::new(
+                        ShaderIROp::Evaluate,
+                        ShaderIRType::I32,
+                        &[sequence_ir, start_ir],
+                        Some(ShaderIREvaluation::ADD),
+                    );
+
+                    VirtualShaderIR::value(ShaderIR::new(
+                        ShaderIROp::Evaluate,
+                        ir_type,
+                        &[offset_ir.clone()],
+                        Some(ShaderIREvaluation::CAST),
+                    ))
+                }
+                InputSpec::Internal(_) | InputSpec::Safetensor(_) => {
+                    VirtualShaderIR::value(ShaderIR::new(
+                        ShaderIROp::Load,
+                        ir_type,
+                        &[index_ir.index.clone()],
+                        Some(ShaderIREvaluation::I32(self.id() as i32)),
+                    ))
+                }
+            }],
+            TensorInput::NoOp(tensor) => vec![VirtualShaderIR::index(
+                index_ir.index.clone(),
+                tensor.clone(),
+            )],
+            TensorInput::OperationResult(result) => result.unroll_ir(index_ir, ir_type),
+            _ => panic!("Unsupported ShaderIR generation for TensorInput"),
+        }
+    }
+}
+
+impl UnrollShaderIR for OperationSpec {
+    fn unroll_ir(&self, inputs: &ShaderIRIndex, ir_type: ShaderIRType) -> Vec<VirtualShaderIR> {
+        match self {
+            OperationSpec::BinaryOp(spec) => spec.unroll_ir(inputs, ir_type),
+            OperationSpec::ReduceOp(spec) => spec.unroll_ir(inputs, ir_type),
+            OperationSpec::UnaryOp(spec) => spec.unroll_ir(inputs, ir_type),
+        }
+    }
+}
+
+impl UnrollShaderIR for UnarySpec {
+    fn unroll_ir(&self, index_ir: &ShaderIRIndex, ir_type: ShaderIRType) -> Vec<VirtualShaderIR> {
+        let index_ir = &index_ir.index;
+        let evaltype = Into::<ShaderIREvaluation>::into(self.op);
+        vec![
+            VirtualShaderIR::op(ir_type, evaltype),
+            VirtualShaderIR::index(
+                self.input.viewtracker().shader_ir(index_ir),
+                self.input.clone(),
+            ),
+        ]
+    }
+}
+
+impl UnrollShaderIR for BinarySpec {
+    fn unroll_ir(&self, index_ir: &ShaderIRIndex, ir_type: ShaderIRType) -> Vec<VirtualShaderIR> {
+        let index_ir = &index_ir.index;
+        let evaltype = Into::<ShaderIREvaluation>::into(self.op);
+        vec![
+            VirtualShaderIR::op(ir_type, evaltype),
+            VirtualShaderIR::index(self.lhs.viewtracker().shader_ir(index_ir), self.lhs.clone()),
+            VirtualShaderIR::index(self.rhs.viewtracker().shader_ir(index_ir), self.rhs.clone()),
+        ]
+    }
+}
+
+impl UnrollShaderIR for ReduceSpec {
+    fn unroll_ir(&self, index_ir: &ShaderIRIndex, ir_type: ShaderIRType) -> Vec<VirtualShaderIR> {
+        let index_ir = &index_ir.index;
+        let evaltype = Into::<ShaderIREvaluation>::into(self.op);
+
+        let view = self.input.view();
+
+        // IR to normalize the index to 0 at AXIS
+        let normalized_ir = {
+            let contiguous_stride_factor = view.shape[self.axis as usize];
+            let zero = ShaderIR::new(
+                ShaderIROp::Const,
+                ShaderIRType::I32,
+                &[],
+                Some(ShaderIREvaluation::I32(0)),
+            );
+
+            let normalized = view
+                .shape
+                .iter()
+                .zip(view.contiguous_stride.iter().zip(view.stride.iter()))
+                .take(self.axis as usize)
+                .fold(
+                    zero.clone(),
+                    |acc, (&shape, (&input_contiguous_stride, &input_stride))| {
+                        let output_contiguous_stride =
+                            input_contiguous_stride / contiguous_stride_factor;
+
+                        let output_contiguous_stride_ir = ShaderIR::new(
+                            ShaderIROp::Const,
+                            ShaderIRType::I32,
+                            &[],
+                            Some(ShaderIREvaluation::I32(output_contiguous_stride as i32)),
+                        );
+                        let shape_ir = ShaderIR::new(
+                            ShaderIROp::Const,
+                            ShaderIRType::I32,
+                            &[],
+                            Some(ShaderIREvaluation::I32(shape as i32)),
+                        );
+                        let input_stride_ir = ShaderIR::new(
+                            ShaderIROp::Const,
+                            ShaderIRType::I32,
+                            &[],
+                            Some(ShaderIREvaluation::I32(input_stride as i32)),
+                        );
+
+                        let x0 = ShaderIR::new(
+                            ShaderIROp::Evaluate,
+                            ShaderIRType::I32,
+                            &[index_ir.clone(), output_contiguous_stride_ir],
+                            Some(ShaderIREvaluation::DIVIDE),
+                        );
+                        // This should not be needed
+                        let x1 = ShaderIR::new(
+                            ShaderIROp::Evaluate,
+                            ShaderIRType::I32,
+                            &[x0, shape_ir],
+                            Some(ShaderIREvaluation::MOD),
+                        );
+                        let x2 = ShaderIR::new(
+                            ShaderIROp::Evaluate,
+                            ShaderIRType::I32,
+                            &[x1, input_stride_ir],
+                            Some(ShaderIREvaluation::MULTIPLY),
+                        );
+
+                        ShaderIR::new(
+                            ShaderIROp::Evaluate,
+                            ShaderIRType::I32,
+                            &[acc, x2],
+                            Some(ShaderIREvaluation::ADD),
+                        )
+                    },
+                );
+
+            let unnormalized = {
+                let contiguous_stride_ir = ShaderIR::new(
+                    ShaderIROp::Const,
+                    ShaderIRType::I32,
+                    &[],
+                    Some(ShaderIREvaluation::I32(
+                        view.contiguous_stride[self.axis as usize] as i32,
+                    )),
+                );
+
+                ShaderIR::new(
+                    ShaderIROp::Evaluate,
+                    ShaderIRType::I32,
+                    &[index_ir.clone(), contiguous_stride_ir],
+                    Some(ShaderIREvaluation::MOD),
+                )
+            };
+
+            ShaderIR::new(
+                ShaderIROp::Evaluate,
+                ShaderIRType::I32,
+                &[normalized, unnormalized],
+                Some(ShaderIREvaluation::ADD),
+            )
+        };
+
+        let reduce_length = view.shape[self.axis as usize];
+        let reduce_stride = view.stride[self.axis as usize];
+
+        let stride_ir = ShaderIR::new(
+            ShaderIROp::Const,
+            ShaderIRType::I32,
+            &[],
+            Some(ShaderIREvaluation::I32(reduce_stride as i32)),
+        );
+
+        let reduce_begin = ShaderIR::new(
+            ShaderIROp::ReduceBegin,
+            ir_type,
+            &[normalized_ir.clone()], // ballmark of reduce region
+            Some(ShaderIREvaluation::I32(reduce_length as i32)),
+        );
+        let adjusted_stride_ir = ShaderIR::new(
+            ShaderIROp::Evaluate,
+            ShaderIRType::I32,
+            &[reduce_begin.clone(), stride_ir],
+            Some(ShaderIREvaluation::MULTIPLY),
+        );
+        let loop_index_ir = ShaderIR::new(
+            ShaderIROp::Evaluate,
+            ShaderIRType::I32,
+            &[normalized_ir.clone(), adjusted_stride_ir],
+            Some(ShaderIREvaluation::ADD),
+        );
+
+        vec![
+            VirtualShaderIR::reduce(reduce_begin, evaltype),
+            VirtualShaderIR::index(
+                self.input.viewtracker().shader_ir(&loop_index_ir),
+                self.input.clone(),
+            ),
+        ]
+    }
+}
+
+impl SerializeShaderIR for TensorViewTracker {
+    fn shader_ir(&self, index_ir: &ShaderIR) -> ShaderIR {
+        let zero = ShaderIR::new(
+            ShaderIROp::Const,
+            ShaderIRType::I32,
+            &[],
+            Some(ShaderIREvaluation::I32(0)),
+        );
+
+        self.serialized_history_fifo()
+            .iter()
+            .fold(index_ir.clone(), |previous_index_ir, view| {
+                view.shape
+                    .iter()
+                    .zip(view.stride.iter().zip(view.contiguous_stride.iter()))
+                    .fold(
+                        zero.clone(),
+                        |partial_index, (&shape, (&stride, &contiguous_stride))| {
+                            let shape_ir = ShaderIR::new(
+                                ShaderIROp::Const,
+                                ShaderIRType::I32,
+                                &[],
+                                Some(ShaderIREvaluation::I32(shape as i32)),
+                            );
+                            let stride_ir = ShaderIR::new(
+                                ShaderIROp::Const,
+                                ShaderIRType::I32,
+                                &[],
+                                Some(ShaderIREvaluation::I32(stride as i32)),
+                            );
+                            let contiguous_stride_ir = ShaderIR::new(
+                                ShaderIROp::Const,
+                                ShaderIRType::I32,
+                                &[],
+                                Some(ShaderIREvaluation::I32(contiguous_stride as i32)),
+                            );
+                            // index / contiguous_stride % shape * stride
+                            let x0 = ShaderIR::new(
+                                ShaderIROp::Evaluate,
+                                ShaderIRType::I32,
+                                &[previous_index_ir.clone(), contiguous_stride_ir],
+                                Some(ShaderIREvaluation::DIVIDE),
+                            );
+                            let x1 = ShaderIR::new(
+                                ShaderIROp::Evaluate,
+                                ShaderIRType::I32,
+                                &[x0, shape_ir],
+                                Some(ShaderIREvaluation::MOD),
+                            );
+                            let x2 = ShaderIR::new(
+                                ShaderIROp::Evaluate,
+                                ShaderIRType::I32,
+                                &[x1, stride_ir],
+                                Some(ShaderIREvaluation::MULTIPLY),
+                            );
+
+                            ShaderIR::new(
+                                ShaderIROp::Evaluate,
+                                ShaderIRType::I32,
+                                &[partial_index, x2],
+                                Some(ShaderIREvaluation::ADD),
+                            )
+                        },
+                    )
+            })
+    }
+}
