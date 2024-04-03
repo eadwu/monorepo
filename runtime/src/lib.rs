17b0a43a665cbd26c64e27a21c259b7f0744ebfb
Edmund Wu <fangkazuto@gmail.com>
2024-04-03 15:27:46 -0400

    runtime: define feature dtensor_shader_collapse

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,1 +1,4 @@
+#[cfg(all(feature = "dtensor_shader_stitch", feature = "dtensor_shader_collapse"))]
+compile_error!("feature \"dtensor_shader_stitch\" and feature \"dtensor_shader_collapse\" cannot be enabled at the same time");
+
 pub mod webgpu;
