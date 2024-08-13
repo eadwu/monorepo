4a032be797cfa81cee13ace641f62e0413d77c5d
Edmund Wu <22758444+eadwu@users.noreply.github.com>
2024-08-12 22:33:34 -0400

    DNS-based load balancing

--- public/tailscale-rpz/main.go
+++ public/tailscale-rpz/main.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 
 	"tailscale.com/atomicfile"
 )
+
+// Tailscale suffixes ephemeral with -N
+// Match everything behind (.*)-N for the central domain for the rest to load balance
+// To make it easier, matching is relaxed to any number of `-` so the name can be
+// "nodename-" and ephemeral instances would be "nodename--1", "nodenname--2"
+// and the central instance is "nodename"
+var EPHERMAL_REGEX *regexp.Regexp = regexp.MustCompile(`([^-]+)-*-[0-9]+`)
 
 const SOA_RECORD string = `
 $TTL 300
@@ -45,6 +53,16 @@ func (t *Tailscale) generateRPZ() string {
 	for host, dns := range t.entries {
 		for record, values := range dns {
 			for _, v := range values {
+				if EPHERMAL_REGEX.MatchString(host) {
+					splits := strings.Split(host, "-")
+					centralAlias := splits[0]
+
+					rpz := fmt.Sprintf("%s.%s	%s	%s\n", centralAlias, t.zone, record, v)
+					if _, err := builder.WriteString(rpz); err != nil {
+						log.Println(err)
+					}
+				}
+
 				rpz := fmt.Sprintf("%s.%s	%s	%s\n", host, t.zone, record, v)
 				if _, err := builder.WriteString(rpz); err != nil {
 					log.Println(err)
